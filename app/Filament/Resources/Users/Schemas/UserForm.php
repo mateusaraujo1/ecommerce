41128de1a8c3897acc2b4889ea2eacb966e00242
mateusaraujo1 <f.mateusaraujo2@gmail.com>
2025-09-06 19:50:48 -0300

    build: filament && crud users

---
+++ app/Filament/Resources/Users/Schemas/UserForm.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Filament\Resources\Users\Schemas;
+
+use Filament\Forms\Components\DateTimePicker;
+use Filament\Forms\Components\TextInput;
+use Filament\Pages\Page;
+use Filament\Schemas\Schema;
+
+class UserForm
+{
+    public static function configure(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                TextInput::make('name')
+                    ->required(),
+                TextInput::make('email')
+                    ->label('Email address')
+                    ->email()
+                    ->maxLength(255)
+                    ->unique(ignoreRecord: true)
+                    ->required(),
+                DateTimePicker::make('email_verified_at')
+                    ->label('Email verified at')
+                    ->default(now()), 
+                TextInput::make('password')
+                    ->password()
+                    ->dehydrated(fn($state) => filled($state))
+                    ->required(fn(Page $livewire): bool => $livewire instanceof \App\Filament\Resources\Users\Pages\CreateUser),
+            ]);
+    }
+}
