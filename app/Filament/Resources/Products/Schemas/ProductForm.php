146673e1f030ec7d0fe21e8489d22321f89fa993
mateusaraujo1 <f.mateusaraujo2@gmail.com>
2025-09-07 12:27:46 -0300

    finish product crud

--- app/Filament/Resources/Products/Schemas/ProductForm.php
+++ app/Filament/Resources/Products/Schemas/ProductForm.php
@@ -2,10 +2,18 @@
 
 namespace App\Filament\Resources\Products\Schemas;
 
+use App\Models\Product;
+use Filament\Forms\Components\FileUpload;
+use Filament\Forms\Components\MarkdownEditor;
+use Filament\Forms\Components\Select;
 use Filament\Forms\Components\TextInput;
 use Filament\Forms\Components\Textarea;
 use Filament\Forms\Components\Toggle;
+use Filament\Schemas\Components\Group;
+use Filament\Schemas\Components\Section;
+use Filament\Schemas\Components\Utilities\Set;
 use Filament\Schemas\Schema;
+use Filament\Support\Markdown;
 
 class ProductForm
 {
@@ -13,31 +21,78 @@ class ProductForm
     {
         return $schema
             ->components([
-                TextInput::make('name')
-                    ->required(),
-                TextInput::make('slug')
-                    ->required(),
-                TextInput::make('price')
-                    ->required()
-                    ->numeric()
-                    ->prefix('$'),
-                Textarea::make('description')
-                    ->columnSpanFull(),
-                TextInput::make('images'),
-                Toggle::make('is_active')
-                    ->required(),
-                Toggle::make('is_featured')
-                    ->required(),
-                Toggle::make('in_stock')
-                    ->required(),
-                Toggle::make('on_sale')
-                    ->required(),
-                TextInput::make('category_id')
-                    ->required()
-                    ->numeric(),
-                TextInput::make('brand_id')
-                    ->required()
-                    ->numeric(),
-            ]);
+                Group::make()->schema([
+                    Section::make('Product Information')->schema([
+                        TextInput::make('name')
+                            ->required()
+                            ->maxLength(255)
+                            ->live(onBlur: true)
+                            ->afterStateUpdated(fn (string $operation, $state, Set $set) => $operation === 'create' ? $set('slug', str()->slug($state)) : null),
+
+                        Textarea::make('slug')
+                            ->required()
+                            ->maxLength(255)
+                            ->disabled()
+                            ->dehydrated()
+                            ->unique(Product::class, 'slug', ignoreRecord: true),
+
+                        MarkdownEditor::make('description')
+                            ->columnSpanFull()
+                            ->fileAttachmentsDirectory('products'),
+                    ])
+                    ->columns(2),
+
+                    Section::make('Images')->schema([
+                        FileUpload::make('images')
+                            ->multiple()
+                            ->directory('products')
+                            ->maxFiles(5)
+                            ->reorderable(),
+                    ])
+                ])
+                ->columnSpan(2),
+
+                Group::make()->schema([
+                    Section::make('Price')->schema([
+                        TextInput::make('price')
+                            ->required()
+                            ->numeric()
+                            ->minValue(0)
+                            ->prefix('R$'),
+                    ]),
+
+                    Section::make('Associations')->schema([
+                        Select::make('category_id')
+                            ->relationship('category', 'name')
+                            ->required()
+                            ->searchable()
+                            ->preload(),
+
+                        Select::make('brand_id')
+                            ->relationship('brand', 'name')
+                            ->required()
+                            ->searchable()
+                            ->preload(),
+                    ]),
+
+                    Section::make('Status')->schema([
+                            Toggle::make('in_stock')
+                                ->required()
+                                ->default(true),
+                                
+                            Toggle::make('is_active')
+                                ->required()
+                                ->default(true),
+                                
+                            Toggle::make('is_featured')
+                                ->required(),
+                                
+                            Toggle::make('on_sale')
+                                ->required(),
+                        ])
+                ])
+                ->columnSpan(1)
+            ])
+            ->columns(3);
     }
 }
--- app/Filament/Resources/Products/Tables/ProductsTable.php
+++ app/Filament/Resources/Products/Tables/ProductsTable.php
@@ -2,11 +2,15 @@
 
 namespace App\Filament\Resources\Products\Tables;
 
+use Filament\Actions\ActionGroup;
 use Filament\Actions\BulkActionGroup;
+use Filament\Actions\DeleteAction;
 use Filament\Actions\DeleteBulkAction;
 use Filament\Actions\EditAction;
+use Filament\Actions\ViewAction;
 use Filament\Tables\Columns\IconColumn;
 use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Filters\SelectFilter;
 use Filament\Tables\Table;
 
 class ProductsTable
@@ -16,40 +20,67 @@ class ProductsTable
         return $table
             ->columns([
                 TextColumn::make('name')
+                    ->alignCenter()
                     ->searchable(),
-                TextColumn::make('slug')
-                    ->searchable(),
+                    
+                TextColumn::make('category.name')
+                    ->alignCenter()
+                    ->sortable(),
+
+                TextColumn::make('brand.name')
+                    ->alignCenter()
+                    ->sortable(),
+
                 TextColumn::make('price')
-                    ->money()
+                    ->alignCenter()
+                    ->money('BRL')
                     ->sortable(),
+
+                TextColumn::make('slug')
+                    ->alignCenter()
+                    ->searchable(),
+
                 IconColumn::make('is_active')
+                    ->alignCenter()
                     ->boolean(),
-                IconColumn::make('is_featured')
-                    ->boolean(),
+
                 IconColumn::make('in_stock')
+                    ->alignCenter()
                     ->boolean(),
+
+                IconColumn::make('is_featured')
+                    ->alignCenter()
+                    ->boolean(),
+
                 IconColumn::make('on_sale')
+                    ->alignCenter()
                     ->boolean(),
-                TextColumn::make('category_id')
-                    ->numeric()
-                    ->sortable(),
-                TextColumn::make('brand_id')
-                    ->numeric()
-                    ->sortable(),
+
                 TextColumn::make('created_at')
+                    ->alignCenter()
                     ->dateTime()
                     ->sortable()
                     ->toggleable(isToggledHiddenByDefault: true),
+                    
                 TextColumn::make('updated_at')
+                    ->alignCenter()
                     ->dateTime()
                     ->sortable()
                     ->toggleable(isToggledHiddenByDefault: true),
             ])
             ->filters([
-                //
+                SelectFilter::make('category')
+                    ->relationship('category', 'name'),
+
+                SelectFilter::make('brand')
+                    ->relationship('brand', 'name'),
             ])
             ->recordActions([
-                EditAction::make(),
+                ActionGroup::make([
+                    ViewAction::make(),
+                    DeleteAction::make(),
+                    EditAction::make(),
+                ]),
             ])
             ->toolbarActions([
                 BulkActionGroup::make([
